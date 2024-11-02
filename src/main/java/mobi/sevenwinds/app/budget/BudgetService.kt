0ca1947bdc286561b5c9b2a2fa531018f2fd80d0
anton <anton4792@tutanota.com>
2024-11-03 03:45:59 +0700

    1-Fix-test statistics calculating fix

--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
@@ -2,7 +2,10 @@ package mobi.sevenwinds.app.budget
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.jetbrains.exposed.sql.count
 import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.sum
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object BudgetService {
@@ -19,22 +22,55 @@ object BudgetService {
         }
     }
 
-    suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
-        transaction {
-            val query = BudgetTable
-                .select { BudgetTable.year eq param.year }
-                .limit(param.limit, param.offset)
+    suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse =
+        newSuspendedTransaction {
+            val yearBudgetRecordsPage = getYearBudgetRecordsPage(param)
+            val (totalOperationsCountForYear, totalAmountByTypes) = getYearStats(param.year)
 
-            val total = query.count()
-            val data = BudgetEntity.wrapRows(query).map { it.toResponse() }
-
-            val sumByType = data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
-
-            return@transaction BudgetYearStatsResponse(
-                total = total,
-                totalByType = sumByType,
-                items = data
+            return@newSuspendedTransaction BudgetYearStatsResponse(
+                total = totalOperationsCountForYear,
+                totalByType = totalAmountByTypes,
+                items = yearBudgetRecordsPage
             )
         }
+
+
+    private fun getYearStats(year: Int): Pair<Int, Map<String, Int>> {
+        val typeColumn = BudgetTable.type
+        val yearColumn = BudgetTable.year
+        val operationsCountForType = BudgetTable.id.count()
+        val operationsAmountSumForType = BudgetTable.amount.sum()
+
+        // Use aggregations in one query to get stats -
+        // all operations count and operations sum by every type for some year
+        val aggregationResult = BudgetTable
+            .slice(typeColumn, operationsCountForType, operationsAmountSumForType)
+            .select { yearColumn eq year }
+            .groupBy(typeColumn)
+            .toList()
+
+        // Sum all operations count
+        val totalOperationsCountForYear = aggregationResult
+            .asSequence()
+            .map { row -> row[operationsCountForType] }
+            .sum()
+
+        val totalAmountByTypes = aggregationResult.associate { row ->
+            val type = row[typeColumn]
+            val totalAmount = row[operationsAmountSumForType] ?: 0
+            type.name to totalAmount
+        }
+
+        return totalOperationsCountForYear to totalAmountByTypes
+    }
+
+    private fun getYearBudgetRecordsPage(param: BudgetYearParam): List<BudgetRecord> {
+        val pageQueryFilteredByYear = BudgetTable
+            .select { BudgetTable.year eq param.year }
+            .limit(param.limit, param.offset)
+
+        return BudgetEntity
+            .wrapRows(pageQueryFilteredByYear)
+            .map(BudgetEntity::toResponse)
     }
 }
