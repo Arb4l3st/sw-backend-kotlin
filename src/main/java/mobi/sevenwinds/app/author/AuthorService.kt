3432719f48388ca5fbf8fac61f4833f3946f537d
anton <podlatovanton@gmail.com>
2024-11-04 23:14:58 +0700

    rollback dispatchersio

--- src/main/java/mobi/sevenwinds/app/author/AuthorService.kt
+++ src/main/java/mobi/sevenwinds/app/author/AuthorService.kt
@@ -1,11 +1,12 @@
 package mobi.sevenwinds.app.author
 
+import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.joda.time.DateTime
 
 object AuthorService {
     suspend fun addAuthor(addAuthorRecordData: AddAuthorRecordData): AuthorRecord =
-        newSuspendedTransaction {
+        newSuspendedTransaction(Dispatchers.IO) {
             AuthorEntity.new {
                 fullName = addAuthorRecordData.fullName
                 creationDateTime = DateTime()
