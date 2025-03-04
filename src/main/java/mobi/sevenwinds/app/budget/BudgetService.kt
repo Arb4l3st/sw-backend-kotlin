dbf2c6a4cbf3d9d3aa9296000de3412882ca821d
Alexander Myasnikov <dart69@vk.com>
2025-03-04 19:56:03 +0300

    Improved code efficiency a lot

--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
@@ -5,24 +5,18 @@ import kotlinx.coroutines.withContext
 import mobi.sevenwinds.app.author.AuthorEntity
 import mobi.sevenwinds.app.author.AuthorTable
 import mobi.sevenwinds.app.utils.firstEntityOrNull
-import org.jetbrains.exposed.sql.JoinType
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.SortOrder
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object BudgetService {
     suspend fun addRecord(body: BudgetRequest): BudgetResponse = withContext(Dispatchers.IO) {
         transaction {
-            val author = AuthorTable
-                .select { AuthorTable.id eq body.authorId }
-                .firstEntityOrNull(AuthorEntity)
-
+            val author = AuthorTable.select { AuthorTable.id eq body.authorId }.firstEntityOrNull(AuthorEntity)
             if (body.authorId != null && author == null) {
-                throw IllegalArgumentException("Can't find author with actual id ${body.authorId}")
+                throw IllegalArgumentException("Can't find author with the actual id ${body.authorId}")
             }
 
-            return@transaction BudgetEntity.new {
+            BudgetEntity.new {
                 this.year = body.year
                 this.month = body.month
                 this.amount = body.amount
@@ -34,21 +28,36 @@ object BudgetService {
 
     suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
         transaction {
-            val query = (BudgetTable leftJoin AuthorTable).select {
-                BudgetTable.year eq param.year
-            }.orderBy(BudgetTable.month to SortOrder.ASC, BudgetTable.amount to SortOrder.DESC)
+            val data = findBudgetStats(param)
+            val (count, sum, type) = arrayOf(BudgetTable.id.count(), BudgetTable.amount.sum(), BudgetTable.type)
+            val totalStats = BudgetTable.slice(count, sum, type).selectAll().groupBy(BudgetTable.type).map { row ->
+                TemporaryStats(
+                    count = row[count] as Int,
+                    sum = row[sum] as Int,
+                    budgetType = row[type] as BudgetType
+                )
+            }
 
-            val data = query.filter { resultRow ->
-                val cleanFilter = param.filter?.trim().orEmpty()
-                val authorName = resultRow.getOrNull(AuthorTable.fullName)
-                param.filter == null || authorName?.contains(other = cleanFilter, ignoreCase = true) == true
-            }.map(ResultRow::toBudgetResponse)
-
-            return@transaction BudgetYearStatsResponse(
-                total = query.count(),
-                totalByType = data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } },
-                items = data.asSequence().drop(param.offset).take(param.limit).toList()
+            BudgetYearStatsResponse(
+                total = totalStats.first { it.budgetType == BudgetType.Приход }.count,
+                totalByType = totalStats.groupBy({ it.budgetType.name }, { it.sum }).mapValues { it.value.sum() },
+                items = data
             )
         }
     }
+
+    fun findBudgetStats(param: BudgetYearParam): List<BudgetResponse> =
+        (BudgetTable leftJoin AuthorTable).select {
+            val mainExpression = BudgetTable.year eq param.year
+            val optionalExpression = (AuthorTable.fullName.lowerCase() like "%${param.filter?.lowercase()}%")
+            param.filter?.let { mainExpression and optionalExpression } ?: mainExpression
+        }.orderBy(
+            BudgetTable.month to SortOrder.ASC, BudgetTable.amount to SortOrder.DESC
+        ).limit(param.limit, param.offset).map(ResultRow::toBudgetResponse)
+
+    private data class TemporaryStats(
+        val count: Int,
+        val sum: Int,
+        val budgetType: BudgetType
+    )
 }
