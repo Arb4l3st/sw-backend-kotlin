98613f1a3206c9e9a01ce489122497764d3a2b9f
Cucumber090-1 <gavroleg7@gmail.com>
2025-03-13 01:52:13 +0300

    add docker-compose & stats calculation

--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
@@ -19,22 +19,55 @@ object BudgetService {
         }
     }
 
-    suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
-        transaction {
-            val query = BudgetTable
+    suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse =
+        newSuspendedTransaction {
+            val yearBudgetRecordsPage = getYearBudgetRecordsPage(param)
+            val (totalOperationsCountForYear, totalAmountByTypes) = getYearStats(param.year)
+
+            return@newSuspendedTransaction BudgetYearStatsResponse(
+                total = totalOperationsCountForYear,
+                totalByType = totalAmountByTypes,
+                items = yearBudgetRecordsPage
+            )
+        }
+
+    private fun getYearStats(year: Int): Pair<Int, Map<String, Int>> {
+        val typeColumn = BudgetTable.type
+        val yearColumn = BudgetTable.year
+        val operationsCountForType = BudgetTable.id.count()
+        val operationsAmountSumForType = BudgetTable.amount.sum()
+
+        // Use aggregations in one query to get stats -
+        // all operations count and operations sum by every type for some year
+        val aggregationResult = BudgetTable
+            .slice(typeColumn, operationsCountForType, operationsAmountSumForType)
+            .select { yearColumn eq year }
+            .groupBy(typeColumn)
+            .toList()
+
+        // Sum all operations count
+        val totalOperationsCountForYear = aggregationResult
+            .asSequence()
+            .map { row -> row[operationsCountForType] }
+            .sum()
+
+        val totalAmountByTypes = aggregationResult.associate { row ->
+            val type = row[typeColumn]
+            val totalAmount = row[operationsAmountSumForType] ?: 0
+            type.name to totalAmount
+        }
+
+        return totalOperationsCountForYear to totalAmountByTypes
+    }
+
+    private fun getYearBudgetRecordsPage(param: BudgetYearParam): List<BudgetRecord> {
+        val operationsByYearPage =
+            BudgetTable
                 .select { BudgetTable.year eq param.year }
                 .limit(param.limit, param.offset)
 
-            val total = query.count()
-            val data = BudgetEntity.wrapRows(query).map { it.toResponse() }
-
-            val sumByType = data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
-
-            return@transaction BudgetYearStatsResponse(
-                total = total,
-                totalByType = sumByType,
-                items = data
-            )
-        }
+        return BudgetEntity
+            .wrapRows(operationsByYearPage)
+            .map(BudgetEntity::toResponse)
     }
 }
