ee73c180e19e7ea8d15c215f3e5aada9dc55139c
Azer Agazade <azer.agazade05@gmail.com>
2025-04-24 22:54:54 +0400

    fixed tests

--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
@@ -1,7 +1,7 @@
 package mobi.sevenwinds.app.budget
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withContext import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -23,12 +23,24 @@ object BudgetService {
         transaction {
             val query = BudgetTable
                 .select { BudgetTable.year eq param.year }
+                .orderBy(BudgetTable.month to SortOrder.ASC, BudgetTable.amount to SortOrder.DESC)
                 .limit(param.limit, param.offset)
 
-            val total = query.count()
+            val total = BudgetTable.select { BudgetTable.year eq param.year }.count()
             val data = BudgetEntity.wrapRows(query).map { it.toResponse() }
 
-            val sumByType = data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
+            val fullData = BudgetTable
+                .select { BudgetTable.year eq param.year }
+                .map {
+                    BudgetRecord(
+                        year = it[BudgetTable.year],
+                        month = it[BudgetTable.month],
+                        amount = it[BudgetTable.amount],
+                        type = it[BudgetTable.type]
+                    )
+                }
+
+            val sumByType = fullData.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
 
             return@transaction BudgetYearStatsResponse(
                 total = total,
